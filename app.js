cc94f3c73f9caeb10df205a4fca55bd772bb7eff
Victor Marin <victormarinviloria@gmail.com>
2021-07-20 07:23:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function changeColor(getColor){
+    let bg = document.querySelector('.bg');
+    let selectColor = getColor.value;
+    let text = getColor.options[getColor.selectedIndex].text;
+    bg.style.background = selectColor; 
+    bg.innerHTML = text;
+}
